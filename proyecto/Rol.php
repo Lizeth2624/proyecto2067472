5261801db7ce29a2a264b53745e16a7e8ea57860
Lizeth2624 <75790541+Lizeth2624@users.noreply.github.com>
2021-02-01 21:00:40 -0500

    Add files via upload
    base de datos

---
+++ proyecto/Rol.php
@@ -0,0 +1,36 @@
+public class Rol {
+
+	public INT Cod_Rol;
+	public VARCHAR Nom_rol;
+
+	/**
+	 * 
+	 * @param Cod_rol
+	 * @param Nom_rol
+	 */
+	public Rol(int Cod_rol, int Nom_rol) {
+		// TODO - implement Rol.Rol
+		throw new UnsupportedOperationException();
+	}
+
+	public void Consultar() {
+		// TODO - implement Rol.Consultar
+		throw new UnsupportedOperationException();
+	}
+
+	public void Modificar() {
+		// TODO - implement Rol.Modificar
+		throw new UnsupportedOperationException();
+	}
+
+	public void Organizar() {
+		// TODO - implement Rol.Organizar
+		throw new UnsupportedOperationException();
+	}
+
+	public void Eliminar() {
+		// TODO - implement Rol.Eliminar
+		throw new UnsupportedOperationException();
+	}
+
+}
